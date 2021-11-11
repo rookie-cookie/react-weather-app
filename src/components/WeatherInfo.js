7f9c62820d0469eda0a31a0fef11feb21d06ece6
Jona Reyes <jonamreyes@gmail.com>
2021-11-11 17:25:37 -0500

    added styling

--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './weatherinfo.css'
 import moment from 'moment'
+import { WiDaySunny } from 'weather-icons-react'
 
 
 export default function WeatherInfo({
@@ -13,16 +14,39 @@ export default function WeatherInfo({
   }) {
 
   return (
-    <div>
-      <p>city: {city}</p>
-      <p>temperature: {temperature}</p>
-      <p>humidity: {humidity} %</p> 
-      <p>windspeed: {windspeed} m/s</p>
-      <p>description: {weatherdesc} </p>
-      <p>day: {moment().format('dddd')} </p>
-      <p>date: {moment().format('LL')}</p>
-      <p>time: {moment().format('h:mm a')}</p>
-      <p>icon: {icon} </p>
+    <div className="weatherinfo-container">
+      <div className="row desc">
+        <div className="col-sm-3 displayicon">
+          <WiDaySunny size={100} color='#555' />
+        </div>
+        <div className="col-sm-9">
+          <p className="displaylocation">{city}</p>
+          <p className="displaydate">{moment().format('dddd')}, {moment().format('LL')} </p>
+          <p className="displaydesc">{weatherdesc} - {moment().format('h:mm a')}</p>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-sm-10">
+          <div className="displaytemp"> {Math.floor(temperature)}&deg;F</div>
+        </div>
+        <div className="col-sm-2">
+          <div className="buttons convertC">C</div>
+          <div className="buttons convertF">F</div>
+        </div>
+      </div>
+      
+     <div className="row">
+       <p className="displayhumidity">humidity: {humidity} %</p> 
+     </div>
+     <div className="row">
+        <p className="displaywind" >windspeed: {windspeed} m/s</p>
+     </div>
+      
+      
+    
+      
+      
 
     </div>
   )

863273e5f940add57212582772107412c7828e94
Jona Reyes <jonamreyes@gmail.com>
2021-11-11 09:41:45 -0500

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { FaMapMarkerAlt } from 'react-icons/fa';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <div className="container weather-body">
+          <div className="weather-header">
+            <h3>Weather Report  <FaMapMarkerAlt /></h3>
+          </div>
+          {/* <Weather /> */}
+       
+        </div>
+        <div className="container ">
+          <div className="weather-footer">
+            <p>Made by Jona Reyes</p>
+          </div>
+        </div>
+      </div>
+       
     </div>
   );
 }
